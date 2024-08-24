669ca82e1a1ad9747e34e159f94adaec403a3991
shubham <shubhamrakhecha5@gmail.com>
2024-08-24 16:19:11 +0530

    using npm

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ app.post('/send/document', upload.single('document'), async (req, res) => {
     }
 });
 
-const port = 8080;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
